33e4a56b5ad14cff86b83f1b679ab8db4825af95
Evgeny Shmakov <sjs-master@yandex.ru>
2024-01-20 17:21:54 +0700

    fix: add bundle-analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  images: {
+    disableStaticImages: true,
+    minimumCacheTTL: 60,
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: ['cdn.pixabay.com', 'media.istockphoto.com'],
+  },
 };
+
+module.exports = withBundleAnalyzer(nextConfig)
